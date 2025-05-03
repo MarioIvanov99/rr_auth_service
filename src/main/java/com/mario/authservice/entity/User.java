18ea8c9434fcfa4034f4d96d210e36caaff8bf08
MarioIvanov99 <marioivanov998@gmail.com>
2025-05-03 09:38:29 +0300

    user entity

--- src/main/java/com/mario/authservice/entity/User.java
+++ src/main/java/com/mario/authservice/entity/User.java
@@ -1,4 +1,32 @@
 package com.mario.authservice.entity;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role authorities;
+
+    private boolean isAccountNonExpired;
+    private boolean isAccountNonLocked;
+    private boolean isCredentialsNonExpired;
+    private boolean isEnabled;
+    private boolean isDeleted;
+
 }
