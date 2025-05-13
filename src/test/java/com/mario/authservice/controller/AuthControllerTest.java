d8c0d629f209ddc0d35a696244d86870591359c6
MarioIvanov99 <marioivanov998@gmail.com>
2025-05-13 10:54:01 +0300

    Began Work on Controller tests

---
+++ src/test/java/com/mario/authservice/controller/AuthControllerTest.java
@@ -0,0 +1,10 @@
+package com.mario.authservice.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+
+
+@WebMvcTest(AuthController.class)
+class AuthControllerTest {
+
+}
